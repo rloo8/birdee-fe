511c6c8cf81ee4483975d23571c2defbef84d095
rloo8 <cc1647@naver.com>
2023-11-09 10:46:01 +0900

    Feat: commonStyles 파일 추가

---
+++ src/styles/commonStyles.js
@@ -0,0 +1,32 @@
+export const solidBtnStyle = `
+background-color: #4d9cd0;
+border: 2px solid #4d9cd0;
+`;
+
+export const stokeBtnStyle = `
+background-color: #fff;
+border: 2px solid #4d9cd0;
+`;
+
+export const btnStyle = `
+padding: 15px 30px;
+font-size: 30px;
+color: #4d9cd0;
+background-color: #f2dd98;
+border-right: 5px solid #4d9cd0;
+border-bottom: 5px solid #4d9cd0;
+`;
+
+export const boxStyle = `
+background-color: #fff;
+border: 2px solid #4d9cd0;
+`;
+
+export const modalBoxStyle = `
+background-color: #edece8;
+border-right: 10px solid #4d9cd0;
+border-bottom: 10px solid #4d9cd0;
+h3 {
+    font-size: 50px;
+  }
+`;
