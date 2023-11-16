2beeda8860f38cb885705952fb92a605dd74ae62
rloo8 <cc1647@naver.com>
2023-11-16 14:42:50 +0900

    Feat: 토큰 유무로 페이지 이동 제한 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Routes/Home";
 import Mypage from "./Routes/Mypage";
 import CreateDiary from "./Routes/CreateDiary";
@@ -10,29 +10,67 @@ import CreateAccount from "./Routes/Auth/CreateAccount";
 import HiddenDiary from "./Routes/Diary/HiddenDiary";
 import DeletedDiary from "./Routes/Diary/DeletedDiary";
 
+// 로그인한 경우에만 허용
+function ProtectedRoute({ element }) {
+  const token = localStorage.getItem("token");
+
+  return token ? element : <Navigate to="/login" />;
+}
+
+// 로그인하지 않은 경우에만 허용
+function PublicRoute({ element }) {
+  const token = localStorage.getItem("token");
+
+  return token ? <Navigate to="/" /> : element;
+}
+
 function App() {
   return (
     <div className="App" style={{ height: "100vh" }}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/mypage" element={<Mypage />}></Route>
+          <Route
+            path="/"
+            element={<ProtectedRoute element={<Home />} />}
+          ></Route>
+          <Route
+            path="/mypage"
+            element={<ProtectedRoute element={<Mypage />} />}
+          ></Route>
 
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/create-account" element={<CreateAccount />}></Route>
+          <Route
+            path="/login"
+            element={<PublicRoute element={<Login />} />}
+          ></Route>
+          <Route
+            path="/create-account"
+            element={<PublicRoute element={<CreateAccount />} />}
+          ></Route>
 
-          <Route path="/diaries/create" element={<CreateDiary />}></Route>
-          <Route path="/diaries/hidden" element={<HiddenDiary />}></Route>
-          <Route path="/diaries/deleted" element={<DeletedDiary />}></Route>
+          <Route
+            path="/diaries/create"
+            element={<ProtectedRoute element={<CreateDiary />} />}
+          ></Route>
+          <Route
+            path="/diaries/hidden"
+            element={<ProtectedRoute element={<HiddenDiary />} />}
+          ></Route>
+          <Route
+            path="/diaries/deleted"
+            element={<ProtectedRoute element={<DeletedDiary />} />}
+          ></Route>
 
-          <Route path="/diaries/:diary_id/pages" element={<PageList />}></Route>
+          <Route
+            path="/diaries/:diary_id/pages"
+            element={<ProtectedRoute element={<PageList />} />}
+          ></Route>
           <Route
             path="/diaries/:diary_id/pages/:page_id"
-            element={<Page />}
+            element={<ProtectedRoute element={<Page />} />}
           ></Route>
           <Route
             path="/diaries/:diary_id/pages/create"
-            element={<CreatePage />}
+            element={<ProtectedRoute element={<CreatePage />} />}
           ></Route>
         </Routes>
       </BrowserRouter>
