b2c1936cdae81e6ff96691641bf89c4901072a25
rloo8 <cc1647@naver.com>
2023-11-28 16:49:27 +0900

    Design: 초대 수락 페이지 style 수정

--- src/Routes/Auth/InviteAccept.js
+++ src/Routes/Auth/InviteAccept.js
@@ -1,16 +1,38 @@
 import axios from "axios";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { HOST_URL } from "../../App";
+import { boxStyle, btnStyle } from "../../styles/commonStyles";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 50vw;
+  padding: 40px;
+  ${boxStyle}
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+`;
+
+const AcceptBtn = styled.button`
+  width: 300px;
+  ${btnStyle}
+`;
 
 const InviteAccept = () => {
   const location = useLocation();
 
   const searchParams = new URLSearchParams(location.search);
   const invitedToken = searchParams.get("token");
-  console.log(invitedToken);
 
   const navigate = useNavigate();
 
+  // 초대 수락 클릭 시
   const onInviteAccept = async () => {
     try {
       const response = await axios.post(
@@ -37,8 +59,22 @@ const InviteAccept = () => {
   };
 
   return (
-    <div>
-      <button onClick={onInviteAccept}>초대 수락</button>
+    <div className="flex justify-center items-center h-[100%]">
+      <Wrapper>
+        <div>
+          <h1 className="text-center text-8xl">BIRDEE</h1>
+          <h2 className="text-center text-3xl">우리들의 교환일기</h2>
+        </div>
+
+        <AcceptBtn onClick={onInviteAccept}>초대 수락</AcceptBtn>
+
+        {/* <div className="flex gap-5 text-xl">
+          <span>로그인 후 수락해주세요</span>
+          <Link to="/login">
+            <span className="text-[#4d9cd0]">Login</span>
+          </Link>
+        </div> */}
+      </Wrapper>
     </div>
   );
 };
